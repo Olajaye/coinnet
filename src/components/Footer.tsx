875899c2f962f670d4945fda41e6aa852798d762
Jayeola Gbolahan <jayeolajeremiah@gmail.com>
2024-11-25 13:21:11 +0100

    coinnet

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,10 +14,15 @@ const Footer = () => {
       <div className="container mx-auto px-4 hidden xl:block">
         <div className="xl:flex relative">
           <div className="max-w-[60%]">
-            <h2 className="font-bold text-4xl text-[#000000] my-5">Spend freely and without limits using Bitcoin through Coinnect.</h2>
-            <h6 className="text-base font-normal text-[#000000] mb-5">With Coinnect, enjoy the freedom to spend Bitcoin without limits. Easily access your digital assets for everyday purchases, secure transfers, and seamless transactions, all within a single, powerful platform. Coinnect makes it easy to leverage the full potential of your Bitcoin, wherever you go.</h6>
+            <h2 className="font-bold text-[#000000] my-5 xl:text-4xl">
+              Spend freely and without limits using Bitcoin through Coinnect.
+            </h2>
+
+            <h6 className="text-base font-normal text-[#000000] mb-5">
+              With Coinnect, enjoy the freedom to spend Bitcoin without limits. Easily access your digital assets for everyday purchases, secure transfers, and seamless transactions, all within a single, powerful platform. Coinnect makes it easy to leverage the full potential of your Bitcoin, wherever you go.
+            </h6>
             <button className="bg-bgyellow w-[300px] p-3 rounded-lg text-lg font-semibold text-foreground my-5">
-             Get It
+              Get It
             </button>
             <div className="flex items-center space-x-7">
               <Image src={'/googleplay.svg'} alt="google play" width={150} height={150}/>
@@ -60,18 +65,24 @@ const Footer = () => {
               <Image src={'/ball.png'} alt="ball" width={500} height={200} className="w-[500px] md:w-[400px]"/>
             </div>
             <div className="xl:max-w-[60%] mt-9 xl:mt-0">
-              <h1>Subscribe</h1>
-              <h2>Stay Informed with Our Newsletter!</h2>
-              <p>Subscribe to our newsletter for the latest updates, exclusive insights, and expert tips delivered right to your inbox. Be the first to know about new features, market trends, and opportunities – all designed to keep you connected and informed.</p>
-              <div>
+              <h1 className="font-semibold text-xl text-[#000000]">Subscribe</h1>
+              <h2 className="text-2xl xl:text-5xl text-[#000000] my-5 font-bold">
+                Stay Informed with Our Newsletter!
+              </h2>
+              <p className="font-normal text-xl text-[#1A1919]" >
+                Subscribe to our newsletter for the latest updates, exclusive insights, and expert tips delivered right to your inbox. Be the first to know about new features, market trends, and opportunities – all designed to keep you connected and informed
+              </p>
+              <div className="text-[#000000]">
                 <input 
                   type="text" 
                   name="" 
-                  id="" 
+                  id=""
+                  placeholder="Enter your Email"
+                  className="w-[50vw] xl:w-[30vw] p-3 rounded-ss-lg rounded-es-lg bg-[#DEDEDE] h-[60px]" 
                 />
                   
-                <button className="bg-bgyellow w-[300px] p-3 rounded-lg text-lg font-semibold text-foreground my-5">
-                  Get It
+                <button className="bg-bgyellow w-[30vw] xl:w-[10vw] px-3 h-[60px] rounded-ee-lg rounded-se-lg text-lg font-semibold text-foreground my-5 border-2 border-black">
+                  Subscribe
                 </button>
               </div>
 
@@ -84,39 +95,40 @@ const Footer = () => {
 
       <div className="container mx-auto px-4 mt-16">
         <div className="xl:flex justify-between">
-          <div className="xl:max-w-[50%]">
+          <div className="xl:max-w-[45%]">
 
-            <div className="flex items-center space-x-3">
+            <div className="flex items-center space-x-5">
              <Image src={'/logo.svg'} alt="logo" width={70} height={70}/>
-             <h2>COINNET</h2>
+             <h2 className="font-extrabold text-4xl text-[#000000]">
+              COINNET
+             </h2>
             </div>
 
-            <p>
+            <p className="mt-7 font-medium text-base text-[#252234]">
               Coinnect is your all-in-one crypto platform, designed to make managing, trading, and spending digital assets simple and secure. With advanced features like multi-currency support, real-time tracking, and seamless transactions, Coinnect empowers you to navigate the crypto landscape confidently. 
-
             </p>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-y-6 lg:gap-y-0 lg:gap-x-9 mt-5 xl:mt-0">
-            <div>
-              <h1>Available Blockchains</h1>
-              <p>Ethereum</p>
-              <p>Binance Smart Chain</p>
-              <p>Polygan</p>
-              <p>Vales</p>
+          <div className="xl:max-w-[52%] grid grid-cols-1 md:grid-cols-3 gap-y-6 lg:gap-y-0 lg:gap-x-3 mt-5 xl:mt-0">
+            <div className="">
+              <h1 className="text-2xl text-[#252234] font-semibold mb-3">Available Blockchains</h1>
+              <p className="text-[#252234] text-lg font-normal mb-2">Ethereum</p>
+              <p className="text-[#252234] text-lg font-normal mb-2">Binance Smart Chain</p>
+              <p className="text-[#252234] text-lg font-normal mb-2">Polygan</p>
+              <p className="text-[#252234] text-lg font-normal mb-2">Vales</p>
             </div>
 
-            <div>
-              <h1>Resources</h1>
-              <p>Documentation</p>
-              <p>Guides</p>
-              <p>Blog</p>
+            <div className="xl:ms-5">
+              <h1 className="text-2xl text-[#252234] font-semibold mb-3">Resources</h1>
+              <p className="text-[#252234] text-lg font-normal mb-2">Documentation</p>
+              <p className="text-[#252234] text-lg font-normal mb-2">Guides</p>
+              <p className="text-[#252234] text-lg font-normal mb-2">Blog</p>
             </div>
 
-            <div>
-              <h1>Company</h1>
-              <p>FAQs</p>
-              <p>About Us</p>
-              <p>Contact Us</p>
+            <div className="">
+              <h1 className="text-2xl text-[#252234] font-semibold mb-3">Company</h1>
+              <p className="text-[#252234] text-lg font-normal mb-2">FAQs</p>
+              <p className="text-[#252234] text-lg font-normal mb-2">About Us</p>
+              <p className="text-[#252234] text-lg font-normal mb-2">Contact Us</p>
             </div>
 
           </div>
@@ -124,14 +136,17 @@ const Footer = () => {
 
         <div className="md:flex justify-between items-center mt-5">
           <div className="grid xl:grid-cols-3 grid-cols-1  gap-4">
-            <p>Contact@Coinnect.com</p>
-            <p className="xl:ms-6">Terms Of Use</p>
-            <p>Privacy Policy</p>
+            <p className="text-[#252234] text-lg font-normal mb-2">Contact@Coinnect.com</p>
+            <div className="md:flex md:space-x-7">
+              <p className="text-[#252234] text-lg font-normal mb-2">Terms Of Use</p>
+              <p className="text-[#252234] text-lg font-normal mb-2">Privacy Policy</p>
+            </div>
+            
           </div>
           <div className="flex space-x-9 mt-4 md:mt-0">
-            <CiBasketball className="w-7 h-7 bg-bgyellow p-1 rounded-lg"/>
-            <SiFacebook className="w-7 h-7 bg-bgyellow p-1 rounded-lg"/>
-            <FaGithub className="w-7 h-7 bg-bgyellow p-1 rounded-lg"/>
+            <CiBasketball className="w-7 h-7 bg-gray-100 p-1 rounded-lg"/>
+            <SiFacebook className="w-7 h-7 bg-gray-100 p-1 rounded-lg"/>
+            <FaGithub className="w-7 h-7 bg-gray-100 p-1 rounded-lg"/>
             <FaInstagram className="w-7 h-7 bg-bgyellow p-1 rounded-lg"/>
 
           </div>
